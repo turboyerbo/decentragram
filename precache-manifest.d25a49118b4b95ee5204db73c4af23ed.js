9bd511d46aa26fc3ab67f347433e624356005740
turboyerbo <30615665+turboyerbo@users.noreply.github.com>
2022-01-17 13:38:32 -0500

    Updates

---
+++ precache-manifest.d25a49118b4b95ee5204db73c4af23ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5415b9da0755964fb92202b48134e3e",
+    "url": "/decentragram/index.html"
+  },
+  {
+    "revision": "5c8699b3ad807b4f3534",
+    "url": "/decentragram/static/css/2.80dce9aa.chunk.css"
+  },
+  {
+    "revision": "c383b4d3fad3b4aef70b",
+    "url": "/decentragram/static/css/main.18de8aef.chunk.css"
+  },
+  {
+    "revision": "5c8699b3ad807b4f3534",
+    "url": "/decentragram/static/js/2.1b347232.chunk.js"
+  },
+  {
+    "revision": "30545a24812bc2557c4d652dede4cf2a",
+    "url": "/decentragram/static/js/2.1b347232.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c383b4d3fad3b4aef70b",
+    "url": "/decentragram/static/js/main.454a99e3.chunk.js"
+  },
+  {
+    "revision": "c792ebfb31db215cc56e",
+    "url": "/decentragram/static/js/runtime-main.24130527.js"
+  },
+  {
+    "revision": "d6cbd8e6d17d679dd3cd7fe3884ac8bb",
+    "url": "/decentragram/static/media/photo.d6cbd8e6.png"
+  }
+]);
